8a0def92870821de469eb2d8f82f1386fa327a69
CoralCodes <coralhidalgo29@gmail.com>
2021-03-10 22:06:56 +0000

    Answers for Problems solved in class

--- main.fs
+++ main.fs
@@ -10,17 +10,33 @@ let rec prod (lst:int list) =
 // Problem 2
 let rec map f (lst:int list) =
     // write your solution here
-    []
-    
+ if List.isEmpty lst then []
+ else (f (List.head lst))::(map f (List.tail lst ))
+
+
+
+    //match lst with
+    //[] -> []
+    //|h::t->(f h)::(map f t)
 
 // Problem 3
 
-let rec odd (lst:int list) =
+let rec odd (lst:int list) = 
     // write your solution here
-     lst=[0;3;5];;
-    
-    
+  if List.isEmpty lst then []
+  else 
+    let h = List.head lst 
+    let t = List.tail lst
+    if h % 2 = 1 then h::(odd t) 
+    else odd t 
+
+
 // Problem 4
-let rec filter f lst =
+let rec filter f (lst:int list) =
     // write your solution here
-    lst
+  if List.isEmpty lst then []
+  else 
+    let h = List.head lst 
+    let t = List.tail lst
+    if f h then h::(filter f t) 
+    else filter f t
